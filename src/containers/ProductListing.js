82877502130674acd091c6252427a07111f6f085
mochay1989 <maychocholin@gmail.com>
2021-05-18 10:27:05 +0630

    Repair_UI

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,10 +1,23 @@
-import react from 'react';
-import {useSelector} from 'react-redux'
+import react,{useEffect}from 'react';
+import {useDispatch, useSelector} from 'react-redux'
 import ProductComponent from './ProductComponent';
 import axios from 'axios';
+import { setProducts} from "../redux/actions/productActions"
 const ProductListing=()=>{
-    const products= useSelector((state) => state)
+    const products= useSelector((state) => state);
+    const dispatch=useDispatch();
     console.log(products)
+    const fetchProducts=async()=>{
+        const response = await axios
+        .get("https://fakestoreapi.com/products")
+        .catch((err)=>{
+            console.log("Err",err);
+        });
+        dispatch(setProducts(response.data));
+    };
+    useEffect(()=>{
+        fetchProducts();
+    },[]);
     return(
         <div className="ui grid container">
            <ProductComponent/>
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,16 +1,12 @@
 import {ActionTypes} from "../actions/productActions"
 
 const initialState={
-    products : [{
-        id:1,
-        title:"Dispesh",
-        category:"programmer"
-    }],
+    products : [],
 };
 export const productReducer=(state=initialState,{type,payload})=>{
     switch(type){
         case ActionTypes.SET_PRODUCT:
-            return state;
+            return {...state,products:payload};
         default:
             return state;
     }
